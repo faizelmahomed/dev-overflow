dcf42fe3fd4f1bfb551beb173a515f3e1b86d5ba
faizelmahomed <118206061+faizelmahomed@users.noreply.github.com>
2023-09-28 04:02:19 +0200

    Added right sidebar

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,3 +1,4 @@
+import RightSidebar from "@/components/shared/RightSidebar";
 import LeftSidebar from "@/components/shared/leftSidebar";
 import Navbar from "@/components/shared/navabar/Navbar";
 import React from "react";
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
+        <RightSidebar />
       </div>
       Toaster
     </main>
